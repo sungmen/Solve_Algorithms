5f24799f1d81021a04e4173667a8dd3149584ece
SeongMen Jeon <spe0678@gmail.com>
2020-04-02 19:23:42 +0900

    Create SWEA1223.cpp

---
+++ SWExpertAcademy/SWEA1223.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	for (int test_case = 1; test_case <= 10; test_case++) {
+        string str, res;
+        int str_len;
+        cin >> str_len;
+        cin >> str;
+        stack<char> stk;
+        for (int i = 0; i < str.length(); i++) {
+            switch(str[i]) {
+                case '+':
+                    while(!stk.empty()) {
+                        res += stk.top();
+                        stk.pop();
+                    }
+                    stk.push(str[i]);
+                    break;
+                case '*':
+                    while(!stk.empty() && stk.top() == '*') {
+                        res += stk.top();
+                        stk.pop();
+                    }
+                    stk.push(str[i]);
+                    break;
+                default:
+                    res += str[i];
+                    break;
+            }
+        }
+        while(!stk.empty()){
+            res += stk.top();
+            stk.pop();
+        }
+        
+        stack<int> stk2;
+		int n1, n2;
+        for(int i = 0; i < str_len; i++) {
+         	switch(res[i]) {
+                case '+':
+                    n1 = stk2.top(); stk2.pop();
+                    n2 = stk2.top(); stk2.pop();
+                    stk2.push(n2 + n1);
+                    break;
+                case '*':
+                    n1 = stk2.top(); stk2.pop();
+                    n2 = stk2.top(); stk2.pop();
+                    stk2.push(n2 * n1);
+                    break;
+                default:
+                    stk2.push(res[i]-48);
+                    break;
+            }
+        }
+
+        int result = stk2.top();
+        cout << "#" << test_case << " " << result << endl;
+    }
+    
+    return 0;
+}
