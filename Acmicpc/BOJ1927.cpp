f791d948614775f36bdce954c774be486f9c89bd
SeongMen Jeon <spe0678@gmail.com>
2020-04-05 20:38:55 +0900

    Create BOJ1927.cpp

---
+++ Acmicpc/BOJ1927.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    priority_queue<int, vector<int>, greater<int> > pq;
+    while(t--) {
+        int num;
+        scanf("%d", &num);
+        switch(num) {
+        case 0:
+            if(pq.empty())
+                puts("0");
+            else {
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+            break;
+        default:
+            pq.push(num);
+            break;
+        }
+    }
+    return 0;
+}
