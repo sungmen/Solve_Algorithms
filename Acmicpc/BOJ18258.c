4222ff059e793ee4604568a56a05ddf216ebe3da
SeongMen Jeon <spe0678@gmail.com>
2020-05-16 18:56:42 +0900

    upload BOJ18258.c

---
+++ Acmicpc/BOJ18258.c
@@ -0,0 +1,38 @@
+#include <cstdio>
+
+int stk[2000001];
+char command[6];
+
+int main() {
+    int N;
+    scanf("%d", &N);
+int front = 0, back = 0, size=0;
+    while(N--) {
+        size = back - front;
+        scanf("%s", command);
+        switch(command[0]){
+        case 'p':
+            if(command[1] == 'u'){
+                scanf("%d", &stk[back++]);
+            } else{
+                printf("%d\n", size?stk[front]:-1);
+                if(size) front++;
+            }
+            break;
+        case 's':
+            printf("%d\n", size);
+            break;
+        case 'e':
+            printf("%d\n", size?0:1);
+            break;
+        case 'f':
+            printf("%d\n", size?stk[front]:-1);
+            break;
+        case 'b':
+            printf("%d\n", size?stk[back-1]:-1);
+            break;
+        }
+    }
+
+    return 0;
+}
