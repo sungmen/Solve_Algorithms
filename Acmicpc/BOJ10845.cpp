d86e80fb33432a81bf8b84d0de6911b00f725bf6
SeongMen Jeon <spe0678@gmail.com>
2020-05-16 18:57:49 +0900

    upload BOJ10845.cpp

--- Acmicpc/BOJ10845.cpp
+++ Acmicpc/BOJ10845.cpp
@@ -1,47 +1,35 @@
-#include <iostream>
-#include <queue>
-using namespace std;
+#include <cstdio>
+
+int stk[10001];
+char command[6];
 
 int main() {
-    int t_case;
-    scanf("%d", &t_case);
-    queue<int> q;
-    while(t_case--) {
-        string command;
-        cin >> command;
-        int n;
-        switch(command[1]) {
-        case 'u':
-            cin >> n;
-            q.push(n);
-            break;
-        case 'o':
-            if(!q.empty()) {
-                printf("%d\n", q.front());
-                q.pop();
-            } else {
-                puts("-1");
+    int N;
+    scanf("%d", &N);
+int front = 0, back = 0, size=0;
+    while(N--) {
+        size = back - front;
+        scanf("%s", command);
+        switch(command[0]){
+        case 'p':
+            if(command[1] == 'u'){
+                scanf("%d", &stk[back++]);
+            } else{
+                printf("%d\n", size?stk[front]:-1);
+                if(size) front++;
             }
             break;
-        case 'i':
-            printf("%d\n", q.size());
+        case 's':
+            printf("%d\n", size);
             break;
-        case 'm':
-            printf("%d\n", q.empty()?1:0);
+        case 'e':
+            printf("%d\n", size?0:1);
             break;
-        case 'r':
-            if(!q.empty()) {
-                printf("%d\n", q.front());
-            } else {
-                puts("-1");
-            }
+        case 'f':
+            printf("%d\n", size?stk[front]:-1);
             break;
-        case 'a':
-            if(!q.empty()) {
-                printf("%d\n", q.back());
-            } else {
-                puts("-1");
-            }
+        case 'b':
+            printf("%d\n", size?stk[back-1]:-1);
             break;
         }
     }
