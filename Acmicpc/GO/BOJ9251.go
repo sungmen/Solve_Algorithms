27752c6fbefb508ecd266b9bc49fd3ce1499b105
sungmen <spe0678@gmail.com>
2020-06-04 08:18:50 +0900

    resolve BOJ9251.go

--- Acmicpc/GO/BOJ9251.go
+++ Acmicpc/GO/BOJ9251.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func LCS(str1, str2 string) int {
-	res := make([][]int, len(str1)+1)
-	for i := range res {
-		res[i] = make([]int, len(str2)+1)
-	}
+	var res [1001][1001]int
 
 	for i := 1; i <= len(str1); i++ {
 		for j := 1; j <= len(str2); j++ {
