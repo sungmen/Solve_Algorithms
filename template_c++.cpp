c22b46e851c13035dc7f7dc91293344bf7b5c0a0
sungmen <spe0678@gmail.com>
2020-06-07 23:41:17 +0900

    upload template_C++

---
+++ template_c++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <stack
+using namespace std;
+typedef long long ll;
+
+int main () {
+
+    return 0;
+}
