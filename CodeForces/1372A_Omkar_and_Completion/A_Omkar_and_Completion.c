d6ce4184d6b4569d483e276e239445a53aafbadd
sungmen <spe0678@gmail.com>
2020-07-12 12:12:27 +0900

    upload A_Omkar_and_Completion.c
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ CodeForces/1372A_Omkar_and_Completion/A_Omkar_and_Completion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        int n;
+        scanf("%d", &n);
+        for (int j = 0; j < n; j++) {
+            printf("1 ");
+        }
+        printf("\n");
+    }
+}
