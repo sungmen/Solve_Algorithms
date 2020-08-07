1548787fe23e1682759404cbd06bdcc28b912c1f
sungmen <spe0678@gmail.com>
2020-08-07 10:49:39 +0900

    edit template_c++.cpp
    change include <bits/stdc++.h>
    
    Signed-off-by: sungmen <spe0678@gmail.com>

--- template_c++.cpp
+++ template_c++.cpp
@@ -1,21 +1,9 @@
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <queue>
-#include <cmath>
-#include <cstring>
-#include <vector>
-#include <map>
-#include <set>
-#include <stack>
-#define endl    '\n'
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-
-int main () {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    return 0;
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    int c;
+    for(cin>>c;c--;){
+        
+    }
 }
