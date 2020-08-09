6e924017e349494477b955d04052837665e6d8a9
sungmen <spe0678@gmail.com>
2020-08-10 08:13:23 +0900

    upload A_Suborrays.cpp
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ CodeForces/1391A_Suborrays/A_Suborrays.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    int t, n;
+    for(cin>>t;t--;) {
+        for (cin>>n;n--;) cout<<n+1<<' ';
+        cout << '\n';
+    }
+}
