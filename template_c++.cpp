e6277c4b6438c3b9798639ea3d2b0967bd917fdc
sungmen <spe0678@gmail.com>
2020-08-31 15:36:59 +0900

    edit template_c+.cpp
    Signed-off-by: sungmen <spe0678@gmail.com>

--- template_c++.cpp
+++ template_c++.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define fest ios_base::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr);
+#define ll long long
+#define ul unsigned long long
+#define lb(x) lower_bound(x)
+#define ub(x) upper_bound(x)
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define endl "\n"
+#define in(x) insert(x)
+#define sz(x) (int)x.size()
+#define loop(x,n) for(int x = 0; x < n; ++x)
+#define rep(x,y,z) for(int x=y;x<=z;x++)
+#define per(x,y,z) for(int x=y;x>=z;x--)
+#define all(x) x.begin(),x.end()
 int main() {
-    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    fest
     int c;
     for(cin>>c;c--;){
         
