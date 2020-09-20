b19c62aa2c9b4dd708e65e9c427f3039d20bbb99
sungmen <spe0678@gmail.com>
2020-09-20 14:01:20 +0900

    upload BOJ9934.cpp
    title : OBILAZAK
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ Acmicpc/C++/BOJ9934.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[1025];
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int k; cin >> k;
+    for (int i = 1; i <= (1<<k)-1; cin>>arr[i], i++);
+    for (int i = k-1; i >= 0; i--) {
+        for (int j = 1<<i; j <= 1<<k; j+=(1<<i+1)) {
+            cout << arr[j] << ' ';
+        }
+        cout << '\n';
+    }
+}
