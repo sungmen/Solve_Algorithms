bdacdce365a8e1d8d717282b1433cc9bb48731e3
sungmen <spe0678@gmail.com>
2020-10-03 00:44:37 +0900

    Upload BOJ6185
    title : Clear And Present Danger
    url : https://www.acmicpc.net/problem/6185
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ Acmicpc/C++/BOJ6185.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int n, m; cin >> n >> m;
+    int v[m];
+    for (int i = 0 ; i < m ; i++) {
+        cin >> v[i];
+    }
+    int d[n+1][n+1];
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> d[i][j];
+        }
+    }
+    for(int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(d[i][k] + d[k][j] < d[i][j]) {
+                    d[i][j] = d[i][k] + d[k][j];
+                }
+            }
+        }
+    }
+    int before = v[0];
+    int res = 0;
+    for (int i = 1; i < m; i++) {
+        int next = v[i];
+        res += d[before][next];
+        before = next;
+    }
+    cout << res;
+    return 0;
+}
