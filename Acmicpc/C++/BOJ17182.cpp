35dd63a24cb46f27e50165483c88d8554dca4410
sungmen <spe0678@gmail.com>
2020-10-06 03:09:58 +0900

    Upload BOJ17182.cpp
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ Acmicpc/C++/BOJ17182.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int d[11][11];
+bool chk[11];
+int ans = INT_MAX, n;
+void dfs(int k, int res, int p) {
+    if (ans < res) return;
+    if(p == n) {
+       if(ans > res) {
+           ans = res;
+       }
+       return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (!chk[i]) {
+            chk[i] = true;
+            dfs(i, res+d[k][i], p+1);
+            chk[i] = false;
+        }
+    }
+    return;
+}
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int k; 
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> d[i][j];
+    for (int m = 0; m < n; m++) for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) if (d[i][m] + d[m][j] < d[i][j]) d[i][j] = d[i][m] + d[m][j];
+    chk[k] = true;
+    dfs(k, 0, 1);    
+    cout << ans;
+    return 0;
+}
