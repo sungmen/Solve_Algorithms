b9a1c3964f03ba7c42a52ff15ca4034b6b215500
sungmen <spe0678@gmail.com>
2020-12-03 08:54:58 +0900

    Edit BOJ1600.cpp
    Signed-off-by: sungmen <spe0678@gmail.com>

--- Acmicpc/C++/BOJ1600.cpp
+++ Acmicpc/C++/BOJ1600.cpp
@@ -6,8 +6,7 @@ int main() {
     int k, w, h; cin >> k >> w >> h;
     int d[h][w];
     for (int i = 0; i < h; i++) for (int j = 0; j < w; j++) cin >> d[i][j];
-    int pos_horse[8][2] = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
-    int pos[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int pos[12][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
     bool chk[k+1][h][w];
     memset(chk, false, sizeof(chk)); 
     queue<pair<pair<int, int>, pair<int, int>>> q;
@@ -20,19 +19,13 @@ int main() {
             cout << cur.first.first << endl;
             return 0;
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 12; i++) {
             pair<pair<int, int>, pair<int, int>> next = {{cur.first.first+1, cur.first.second},  {cur.second.first + pos[i][0], cur.second.second + pos[i][1]}};
+            if (i > 3 && !cur.first.second) break;
+            if (i > 3) next.first.second-=1;
             if (next.second.first < 0 || next.second.first >= h || next.second.second < 0 || next.second.second >= w || d[next.second.first][next.second.second] || chk[next.first.second][next.second.first][next.second.second]) continue;
             chk[next.first.second][next.second.first][next.second.second] = 1;
             q.push({{next.first.first, next.first.second}, {next.second.first, next.second.second}});
-        }
-        if (cur.first.second) {
-            for (int i = 0; i < 8; i++) {
-                pair<pair<int, int>, pair<int, int>> next = {{cur.first.first+1, cur.first.second-1}, {cur.second.first + pos_horse[i][0], cur.second.second + pos_horse[i][1]}};
-                if (next.second.first < 0 || next.second.first >= h || next.second.second < 0 || next.second.second >= w || d[next.second.first][next.second.second] || chk[next.first.second][next.second.first][next.second.second]) continue;
-                chk[next.first.second][next.second.first][next.second.second] = 1;
-                q.push({{next.first.first, next.first.second}, {next.second.first, next.second.second}});
-            }
         }
     }
     cout << -1 << endl;
