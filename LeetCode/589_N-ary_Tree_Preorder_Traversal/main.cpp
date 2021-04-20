d8804bb4034cf031228781b674a45825c5fe9329
sungmen <spe0678@gmail.com>
2021-04-21 00:46:51 +0900

    Upload 589. N-ary Tree Preorder Traversal / main.cpp
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/589_N-ary_Tree_Preorder_Traversal/main.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution 
+{
+private:
+    vector<int> res;
+    
+public:
+    void preorderVec(Node * root) 
+    {
+        res.push_back(root->val);
+        
+        for (auto n : root->children) 
+        {
+            preorderVec(n);
+        }
+    }
+    
+    vector<int> preorder(Node* root) 
+    {
+        if (root != nullptr) 
+        {
+            preorderVec(root);
+        }
+        
+        return res;
+    }
+};
