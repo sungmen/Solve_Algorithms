e4782c2ba6affc0d70d41bb05531a4f003aac39d
SeongMen Jeon <spe0678@gmail.com>
2021-10-20 06:23:11 +0900

    Update BOJ11559.cpp

--- Acmicpc/C++/BOJ11559.cpp
+++ Acmicpc/C++/BOJ11559.cpp
@@ -2,37 +2,35 @@
 using namespace std;
 const int pos[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 int main() {
-    //freopen("input.in", "r", stdin);
+    // freopen("input.in", "r", stdin);
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    char str[12][6];
+    string str[12];
     for (int i = 0; i < 12; ++i) {
-        for (int j = 0; j < 6; ++j) {
-            cin >> str[i][j];
-        }
+        cin >> str[i];
     }
     int res = 0;
     bool chk = true;
     vector<pair<int,int>> v;
-    while(1) {
+    while(chk) {
         chk = false;
-        for (int i = 11; i >= 0; --i) {
-            for (int j = 0; j < 6; ++j) {
-                if (str[i][j] != '.') {
+        vector<vector<bool>> schk(12, vector<bool>(6, false));
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (str[i][j] != '.' && !schk[i][j]) {
                     v.clear();
-                    vector<vector<int>> schk(12, vector<int>(6, -1));
                     queue<pair<int,int>> q;
                     q.push({i ,j});
                     v.push_back({i, j});
-                    schk[i][j] = 0;
+                    schk[i][j] = 1;
                     while (!q.empty()) {
                         pair<int,int> cur = q.front();
                         q.pop();
                         for (int k = 0; k < 4; ++k) {
                             pair<int,int> next = {cur.first + pos[k][0], cur.second + pos[k][1]};
-                            if (next.first >= 0 && next.first < 12 && next.second >= 0 && next.second < 6 && schk[next.first][next.second] == -1 && str[next.first][next.second] == str[i][j]) {
-                                schk[next.first][next.second] = schk[cur.first][cur.second] + 1;
-                                v.push_back(next);
+                            if (next.first >= 0 && next.first < 12 && next.second >= 0 && next.second < 6 && !schk[next.first][next.second] && str[next.first][next.second] == str[i][j]) {
                                 q.push(next);
+                                schk[next.first][next.second] = 1;
+                                v.push_back(next);
                             }
                         }
                     }
@@ -45,10 +43,6 @@ int main() {
                 }
             }
         }
-        if (chk) {
-            ++res;
-        }
-        bool stop = false;
         for (int i = 11; i >= 0; --i) {
             for (int j = 0; j < 6; ++j) {
                 if (str[i][j] != '.') {
@@ -56,7 +50,6 @@ int main() {
                     while (y < 11) {
                         if (str[y + 1][x] == '.') {
                             swap(str[y + 1][x], str[y][x]);
-                            stop = true;
                             y = y + 1;
                         } else {
                             break;
@@ -65,7 +58,9 @@ int main() {
                 }
             }
         }
-        if (!stop) break;
+        if (chk) {
+            ++res;
+        }
     }
     cout << res;
     return 0;
