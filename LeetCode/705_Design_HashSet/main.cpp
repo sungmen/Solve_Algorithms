e9dd1ed96fe9ced1b8a8a6b661a68e1123ebb1bd
sungmen <spe0678@gmail.com>
2021-11-14 18:19:13 +0900

    UPLOAD LEETCODE 705
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/705_Design_HashSet/main.cpp
@@ -0,0 +1,25 @@
+class MyHashSet {
+public:
+    map<int, int> m;
+    MyHashSet() {}
+    
+    void add(int key) {
+        m[key] = 1;
+    }
+    
+    void remove(int key) {
+        m.erase(key);
+    }
+    
+    bool contains(int key) {
+        return (m.find(key) != m.end());
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
