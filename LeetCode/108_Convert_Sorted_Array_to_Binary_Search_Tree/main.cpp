c0ae1ab0194b7a1560c9e2bbb95a3df833febbdf
sungmen <spe0678@gmail.com>
2022-01-28 08:36:00 +0900

    UPLOAD LEETCODE 108
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/108_Convert_Sorted_Array_to_Binary_Search_Tree/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:    
+    TreeNode* sortedArrayToBST(vector<int>& nums, int l, int r) {
+        if (l >= r) {
+            return nullptr;
+        }
+        
+        int mid = (l + r) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        
+        root->left = sortedArrayToBST(nums, l, mid);
+        root->right = sortedArrayToBST(nums, mid + 1, r);
+            
+        return root;
+    }
+    
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBST(nums, 0, nums.size());
+    }
+};
