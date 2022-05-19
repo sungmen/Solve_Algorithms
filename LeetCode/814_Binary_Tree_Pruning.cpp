374899b6dff8c4854dcfe3f4b48a710fef49a054
sungmen <spe0678@gmail.com>
2022-05-20 06:33:54 +0900

    UPLOAD LEETCODE 814
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/814_Binary_Tree_Pruning.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    TreeNode* pruneTree(TreeNode* root) 
+    {
+        if (root->left) 
+        {
+            root->left = pruneTree(root->left);
+        }
+        else
+        {
+            root->left = nullptr;      
+        }
+        if (root->right) 
+        {
+            root->right = pruneTree(root->right);            
+        }
+        else
+        {
+            root->right = nullptr;
+        }
+        
+        if (!root->left && !root->right && !root->val) return nullptr;
+        return root;
+    }
+};
