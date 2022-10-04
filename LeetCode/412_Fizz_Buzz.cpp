30027f93492a51a1a495a88a9e946a13242c3ad3
sungmen <spe0678@gmail.com>
2022-10-05 04:05:01 +0900

    UPLOAD LEETCODE 412
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/412_Fizz_Buzz.cpp
@@ -0,0 +1,28 @@
+class Solution 
+{
+public:
+    vector<string> fizzBuzz(int n) 
+    {
+        vector<string> res;
+        for (int i = 1; i <= n; i++)
+        {
+            if (! (i % 15))
+            {
+                res.push_back("FizzBuzz");
+            }
+            else if (! (i % 5))
+            {
+                res.push_back("Buzz");
+            }
+            else if (! (i % 3))
+            {
+                res.push_back("Fizz");
+            }
+            else
+            {
+                res.push_back(to_string(i));
+            }
+        }
+        return res;
+    }
+};
