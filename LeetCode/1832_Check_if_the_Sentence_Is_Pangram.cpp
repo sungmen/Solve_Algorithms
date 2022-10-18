3d168cbbd5e9050e006c23cc7a187adf3221ad07
sungmen <spe0678@gmail.com>
2022-10-18 19:37:23 +0900

    UPLOAD LEETCODE 1832
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/1832_Check_if_the_Sentence_Is_Pangram.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+public:
+    bool checkIfPangram(string sentence) 
+    {
+        set<char> s;
+        for (auto ch : sentence)
+        {
+            s.insert(ch);
+        }
+        return s.size() == 26 ? 1 : 0;
+    }
+};
