a85e655b5dfe2be84528b63ee35761cd2d8260e9
sungmen <spe0678@gmail.com>
2022-12-18 12:53:24 +0900

    UPLOAD LEETCODE 2433
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/2433_Find_The_Original_Array_of_Prefix_Xor.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    vector<int> findArray(vector<int>& pref) 
+    {
+        vector<int> res;
+
+        res.push_back(pref[0]);
+        
+        for (int i = 0; i < pref.size() - 1; i++)
+        {
+            res.push_back(pref[i] ^ pref[i + 1]);
+        }
+        
+        return res;
+    }
+};
