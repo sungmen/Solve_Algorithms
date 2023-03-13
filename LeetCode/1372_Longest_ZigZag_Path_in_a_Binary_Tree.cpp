35bc98b1627a9e02148faf46ffeb2b46a8532c62
sungmen <spe0678@gmail.com>
2023-03-13 20:35:59 +0900

    UPLOAD LEETCODE 1372
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/1372_Longest_ZigZag_Path_in_a_Binary_Tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+private:
+    int ans = 0;
+
+public:
+
+    void dfs(TreeNode* root, int res, int lr)
+    {
+        if (nullptr == root)
+        {
+            ans = max(ans, res);
+            return;
+        }
+        dfs(root->left, (2==lr?res+1:0), 1);
+        dfs(root->right, (1==lr?res+1:0), 2);
+    }
+
+    int longestZigZag(TreeNode* root) 
+    {
+        dfs(root, 0, 0);
+        return ans;
+    }
+};
