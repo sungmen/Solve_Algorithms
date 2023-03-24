57f88471d9b15db5d5e8fe3afb567ce73a40937b
sungmen <spe0678@gmail.com>
2023-03-25 00:26:47 +0900

    UPLOAD LEETCODE 1466
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        map<pair<int, int>, int> connec;
+        unordered_map<int, vector<int>> um;
+        for (auto connect : connections)
+        {
+            connec[{connect[0], connect[1]}] = 1;
+            um[connect[0]].push_back(connect[1]);
+            um[connect[1]].push_back(connect[0]);
+        }
+        queue<int> q;
+        q.push(0);
+        vector<int> chk(n, false);
+        chk[0] = 1;
+        int res = 0;
+        while (! q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+            for (auto ne : um[cur])
+            {
+                if (chk[ne]) continue;
+                if (connec[{cur, ne}]) ++res;
+                chk[ne] = 1;
+                q.push(ne);
+            }
+        }
+        return res;
+    }
+};
