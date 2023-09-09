c581eef86b2f90603716edae021410db063d00e9
sungmen <spe0678@gmail.com>
2023-09-10 00:19:14 +0900

    UPLOAD LEETCODE 377
    Signed-off-by: sungmen <spe0678@gmail.com>

---
+++ LeetCode/377_Combination_Sum_IV.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int combinationSum4(vector<int>& nums, int target) 
+    {
+        vector<unsigned long long> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++)
+        {
+            for (auto j : nums)
+            {
+                if (i < j) continue;
+                dp[i] += dp[i - j];
+            }
+        }
+
+        return dp[target];
+    }
+};
