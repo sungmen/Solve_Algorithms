faef9defba987b4555e86c7ca80d386926f24a41
sungmen <spe0678@gmail.com>
2024-01-05 00:03:49 +0900

    UPLOAD LEETCODE 2125
    Signed-off-by: sungmen <spe0678@gmail.com>

--- LeetCode/2125_Number_of_Laser_Beams_in_a_Bank.cpp
+++ LeetCode/2125_Number_of_Laser_Beams_in_a_Bank.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int numberOfBeams(vector<string>& bank) {
-        vector<int> v;
-        for (auto str : bank) {
-            int tmp = 0;
-            for (auto c : str) {
-                if (c == '1') tmp++;
-            }
-            if (tmp) v.push_back(tmp);
-        }
-        int answer = 0;
-        for(int i = 1; i < v.size(); i++) {
-            answer += (v[i] * v[i-1]);
-        }
-        return answer;
+  int numberOfBeams(vector<string> &bank) {
+    int back = 0;
+    int ans = 0;
+    for (auto s : bank) {
+      int cnt = 0;
+      for (auto c : s) {
+        if ('1' == c)
+          cnt++;
+      }
+      if (cnt == 0)
+        continue;
+      if (back == 0) {
+        back = cnt;
+        continue;
+      }
+      ans += back * cnt;
+      back = cnt;
     }
+    return ans;
+  }
 };
