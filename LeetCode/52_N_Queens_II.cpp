3853cc503e0030f4c9a6b8fc7d800e5c4cae4179
SeongMen Jeon <spe0678@gmail.com>
2025-05-10 00:57:24 +0900

    UPLOAD LEETCODE 52
    Signed-off-by: SeongMen Jeon <spe0678@gmail.com>

---
+++ LeetCode/52_N_Queens_II.cpp
@@ -0,0 +1,61 @@
+class Solution 
+{
+public:
+    vector<string> changeSlashToDot(const int n, vector<string> vec)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (vec[i][j] == '-')
+                {
+                    vec[i][j] = '.';
+                }
+            }
+        }
+        return vec;
+    }
+
+    void makeChess(const int n, const int r, const int c, vector<string> vec, int& answer)
+    {
+        if (n == r + 1)
+        {
+            answer++;
+            return;
+        }
+
+        int r1 = r + 1;
+        int c1 = c;
+        while (r1 < n && c1 < n) vec[r1++][c1] = '-';
+        int r2 = r + 1;
+        int c2 = c + 1;
+        while (r2 < n && c2 < n) vec[r2++][c2++] = '-';
+        int r3 = r + 1;
+        int c3 = c - 1;
+        while (r3 < n && c3 >= 0) vec[r3++][c3--] = '-';
+        for (int i = 0; i < n; i++)
+        {
+            if (vec[r + 1][i] != '-')
+            {
+                vec[r + 1][i] = 'Q';
+                makeChess(n, r + 1, i, vec, answer);
+                vec[r + 1][i] = '.';
+            }
+        }
+    }
+
+
+    int totalNQueens(int n) 
+    {
+        int answer = 0;
+        string strChess(n, '.');
+        vector<string> vec(n, strChess);
+        for (int i = 0; i < n; i++)
+        {
+            vec[0][i] = 'Q';
+            makeChess(n, 0, i, vec, answer);
+            vec[0][i] = '.';
+        }
+        return answer;
+    }
+};
